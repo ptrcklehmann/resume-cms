884310d2757f91956e8b90283a51fa22b552a08d
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-03-19 20:12:32 +0100

    fix sea typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const Resume = () => (
       <p className="pt-6">
       <span role='img' aria-label='beers'>🍻</span>Cheers, I am <span className='text-lime'>Patrick</span>,</p>
       <p className="pt-6">
-      A detail-oriented frontend developer from <span className='text-lime'>rural Brazil</span>, currently living in ~~Berlin~~ on the road. I've transitioned from graphic design to web development in the past 3 years, after helping <span className='text-lime'>organisations</span> like ILO and Aviatrix deliver <span className='text-lime'>kick-ass</span> campaigns, worked as a barista to level-up my <span className='text-lime'>German</span>skills, backpacked 3 months through Europe with my dog <span className='text-lime'>Ponyo</span> and tent Priscila, discovering the southern beaches of Portugal and cruising the cold waters of the Baltic See.
+      A detail-oriented frontend developer from <span className='text-lime'>rural Brazil</span>, currently living in ~~Berlin~~ on the road. I've transitioned from graphic design to web development in the past 3 years, after helping <span className='text-lime'>organisations</span> like ILO and Aviatrix deliver <span className='text-lime'>kick-ass</span> campaigns, worked as a barista to level-up my <span className='text-lime'>German</span>skills, backpacked 3 months through Europe with my dog <span className='text-lime'>Ponyo</span> and tent Priscila, discovering the southern beaches of Portugal and cruising the cold waters of the Baltic Sea.
       </p>
       <p className="pt-6">
       I have <span className='text-lime'>3 years</span> of experience as a <span className='text-lime'>developer</span> and love working with <span className='text-lime'>React</span> and other hip frameworks.
