7da352b74481af0196079b13b8cc71d3f4ddf7c9
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-04-05 19:51:59 +0200

    homepage styling body border change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,12 +33,16 @@ weekday[6] = "Saturday";
 const dayname = weekday[day.getDay()]
 
 const Resume = () => (
-  <main className="antialiased text-black bg-white min-h-screen sm:p-5">
+  <main className="antialiased text-black bg-neutral-100 min-h-screen p-12 lg:p-32">
+    <div id="left"></div>
+    <div id="right"></div>
+    <div id="top"></div>
+    <div id="bottom"></div>
     <SEO title="ptrcklehmann" />
-    <div className="index min-w-screen whitespace-normal m-auto shadow bg-neutral-100 p-2 lg:p-12 px-3 lg:px-46 text-4xl lg:text-6xl">
+    <div className="index min-w-screen whitespace-normal m-auto bg-neutral-100 p-2 lg:p-12 px-3 lg:px-46 text-4xl lg:text-6xl">
       <p className="pt-6">
-        <span role='img' aria-label='beers'>🍻</span>Cheers, I am <span className='text-lime'>Patrick</span>,</p>
-      <p className="pt-6">
+        <span role='img' aria-label='beers'>🍻</span>Cheers, I am <span className='text-lime'>Patrick</span>,<br/>
+      
         A detail-oriented frontend developer from <span className='text-lime'>rural Brazil</span>, currently living in ~~Berlin~~ on the road. I've transitioned from graphic design to web development in the past 3 years, after helping <span className='text-lime'>organisations</span> like the <a href="https://www.ilo.org/" className='text-lime'>ILO</a> and <a href="https://www.aviatrixberlin.com/" className='text-lime'>Aviatrix</a> deliver <span className='text-lime'>kick-ass</span> campaigns, worked as a barista to level-up my <span className='text-lime'>German</span> skills, backpacked 3 months through Europe with my dog <span className='text-lime'>Ponyo</span> <span role='img' aria-label='dog'>🐕</span> and tent <span className='text-lime'>Priscila</span> <span role='img' aria-label='tent'>⛺️</span>, discovering the southern beaches of Portugal and cruising the cold waters of the Baltic Sea.
       </p>
       <p className="pt-6">
@@ -53,26 +57,26 @@ const Resume = () => (
       have a <TextScramble className='inline' texts={scrambleTexts} symbols={scrambleTexts} letterSpeed={2} nextLetterSpeed={20} pauseTime={2000} paused={false} /> {dayname}.</span>
       </div>
       <footer>
-        <div className='flex justify-end pt-8'>
-        <AniLink cover direction='top' bg="#6CF266" to="/resume" className='bg-white tracking-wide text-lime rounded border-b-2 border-lime hover:border-lime hover:bg-lime hover:text-black shadow-md py-2 px-2 inline-flex items-center text-1xl lg:text-4xl'>
-          <span className="mx-auto">
-            <ResumeIcon className="index-icon" />
-            </span>
-        </AniLink>
-        {resume.social.map(item => (
-          <a
-            key={item.service}
-            className='bg-white ml-2 tracking-wide text-lime rounded border-b-2 border-lime hover:border-lime hover:bg-lime hover:text-black shadow-md py-2 px-2 inline-flex items-center text-1xl lg:text-4xl'
-            href={item.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            title={item.service}
-          >
+        <div className='flex justify-start pt-8'>
+          <AniLink cover direction='top' bg="#6CF266" to="/resume" className='bg-white tracking-wide text-lime rounded border-b-2 border-lime hover:border-lime hover:bg-lime hover:text-black shadow-md py-2 px-2 inline-flex items-center text-1xl lg:text-4xl'>
             <span className="mx-auto">
-            <SocialIcon type={item.service} />
+              <ResumeIcon className="index-icon" />
             </span>
-          </a>
-        ))}
+          </AniLink>
+          {resume.social.map(item => (
+            <a
+              key={item.service}
+              className='bg-white ml-2 tracking-wide text-lime rounded border-b-2 border-lime hover:border-lime hover:bg-lime hover:text-black shadow-md py-2 px-2 inline-flex items-center text-1xl lg:text-4xl'
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              title={item.service}
+            >
+              <span className="mx-auto">
+                <SocialIcon type={item.service} />
+              </span>
+            </a>
+          ))}
         </div>
       </footer>
     </div>
